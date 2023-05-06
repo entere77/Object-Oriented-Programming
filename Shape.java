59b97d47f4adca148d14602bebcdc73416a0d456
JJong <63280700+entere77@users.noreply.github.com>
2023-05-06 12:52:04 +0900

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,13 @@
+public class Shape {
+    public double getArea(){
+        return 0.00;
+    }
+
+    public double getPerimeter(){
+        return 0.00;
+    }
+
+    public String toString(){
+        return "Shape";
+    }
+}
